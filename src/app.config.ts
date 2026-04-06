d85d5a1e09ea6893305fe4669db09bd40cdbcfa6
王权 <17612172372@163.com>
2026-04-06 23:35:47 +0800

    fix: 底部tarbar图标修复

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,46 +1,46 @@
 export default defineAppConfig({
   pages: [
-    'pages/home/index',
-    'pages/category/index',
-    'pages/cart/index',
-    'pages/profile/index'
+    "pages/home/index",
+    "pages/category/index",
+    "pages/cart/index",
+    "pages/profile/index",
   ],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
   },
   tabBar: {
-    color: '#999999',
-    selectedColor: '#ff6a00',
-    backgroundColor: '#ffffff',
-    borderStyle: 'black',
+    color: "#999999",
+    selectedColor: "#ff6a00",
+    backgroundColor: "#ffffff",
+    borderStyle: "black",
     list: [
       {
-        pagePath: 'pages/home/index',
-        text: '首页',
-        iconPath: 'assets/tabbar/home.png',
-        selectedIconPath: 'assets/tabbar/home-active.png'
+        pagePath: "pages/home/index",
+        text: "首页",
+        iconPath: "assets/tabbar/home.png",
+        selectedIconPath: "assets/tabbar/home-active.png",
       },
       {
-        pagePath: 'pages/category/index',
-        text: '分类',
-        iconPath: 'assets/tabbar/category.png',
-        selectedIconPath: 'assets/tabbar/category-active.png'
+        pagePath: "pages/category/index",
+        text: "分类",
+        iconPath: "assets/tabbar/category.png",
+        selectedIconPath: "assets/tabbar/category-active.png",
       },
       {
-        pagePath: 'pages/cart/index',
-        text: '购物车',
-        iconPath: 'assets/tabbar/cart.png',
-        selectedIconPath: 'assets/tabbar/cart-active.png'
+        pagePath: "pages/cart/index",
+        text: "购物车",
+        iconPath: "assets/tabbar/cart.png",
+        selectedIconPath: "assets/tabbar/cart-active.png",
       },
       {
-        pagePath: 'pages/profile/index',
-        text: '我的',
-        iconPath: 'assets/tabbar/profile.png',
-        selectedIconPath: 'assets/tabbar/profile-active.png'
-      }
-    ]
-  }
-})
+        pagePath: "pages/profile/index",
+        text: "我的",
+        iconPath: "assets/tabbar/profile.png",
+        selectedIconPath: "assets/tabbar/profile-active.png",
+      },
+    ],
+  },
+});
